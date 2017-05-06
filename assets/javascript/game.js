c9cc04d2e2b6a940b2eb78f615e817385beb5f8b
YETC7 <tyler_726@live.com>
2017-05-06 12:26:12 -0700

    records the guesses from the user

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,43 @@
 
+	// Declaring Variables
 	var alphabet = "qwertyuiopasdfghjklzxcvbnm";
+	var winNum = 0;
+	var loseNum = 0;
+	var userGuess = "";
+		document.getElementById("userGuess").textContent = userGuess;
+	var maxGuess = 10;
+	var guessesLeft = maxGuess;
+		document.getElementById("guessesLeft").textContent = guessesLeft;
+	var gameOver = false;
+
+	// Generates random letter to guess
 	var rand = Math.floor((Math.random()*26));
 	var letter = alphabet.charAt(rand);
 	console.log(letter);
 
-	var test = "test";
-
-	var winNum = "0";
-	var loseNum = "0";
-	var userGuess = "";
-	var maxGuess = "10";
-
 	document.onkeyup = function(event) {
 		var userLetter = event.key;
-		console.log(userLetter);
+
+		// Checks for valid input
+		if(alphabet.indexOf(userLetter) == -1) {
+				return;
+		}
+
+		// Game does not continue if user has no guesses left
+		if (guessesLeft > 0) {
+
+			console.log(userLetter);
+			
+			if (guessesLeft != maxGuess) {
+				userGuess = userGuess + ",";
+			}
+
+			// Prints strings of guesses
+			userGuess = userGuess + userLetter;
+			document.getElementById("userGuess").textContent = userGuess;
+
+			// Decrease the amount of guesses the user has
+			guessesLeft--;
+			document.getElementById("guessesLeft").textContent = guessesLeft;
+		}
 	}
